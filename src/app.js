36a8230143f7001b3fa537350dc7ac5154528216
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-05-10 18:38:15 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
 };
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+  ];
+
+  let theWho = who[Math.floor(Math.random() * who.length)];
+  let theAction = action[Math.floor(Math.random() * action.length)];
+  let theWhat = what[Math.floor(Math.random() * what.length)];
+  let theWhen = when[Math.floor(Math.random() * when.length)];
+
+  return theWho + " " + theAction + " " + theWhat + " " + theWhen + "!";
+}
